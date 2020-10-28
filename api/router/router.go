4b5eda04b11247f492f6c78da2aad7fe93db8533
Ēriks Vītoliņš <vitolinseriks@gmail.com>
2020-10-28 19:20:16 +0200

    small fixes left over from formatting

--- api/router/router.go
+++ api/router/router.go
@@ -1,10 +1,11 @@
-package server
+package router
 
 import (
 	"api/controllers/user"
 	"api/helpers/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"net/http"
@@ -17,29 +18,52 @@ func Init() {
 }
 
 func setupRoutes(app *fiber.App) {
+	routeMiddlewareStart(app)
+
+	authRoutes(app)
+
+	api := app.Group("/api", middleware.Protected())
+	admin := api.Group("/admin", func(context *fiber.Ctx) error {
+		return middleware.IsAdmin(context)
+	})
+
+	adminRoutes(admin)
+	apiRoutes(api)
+
+	routeMiddlewareAfter(app)
+}
+
+func authRoutes(app *fiber.App) {
+	app.Post("/auth/login", user.Login)
+}
+
+func adminRoutes(admin fiber.Router) {
+	admin.Post("/users", user.Index)
+	admin.Post("/user/:id/lock", user.LockUser)
+	admin.Post("/user/:id/unlock", user.UnlockUser)
+}
+
+func apiRoutes(api fiber.Router) {
+	api.Get("/user/:id/profile", user.User)
+}
+func routeMiddlewareStart(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "*",
 		AllowHeaders:     "Content-Type, Authorization, Content-Length, X-Requested-With, Accept",
 		AllowMethods:     "GET, POST, OPTIONS",
 		AllowCredentials: true,
 	}))
+	app.Use(favicon.New())
 	app.Use(logger.New())
 
-	app.Post("/auth/login", user.Login)
+}
 
-	api := app.Group("/api", middleware.Protected())
-	admin := api.Group("/admin", func(context *fiber.Ctx) error {
-		return middleware.IsAdmin(context)
-	})
-	admin.Post("/users", user.Index)
-	api.Get("/user/:id/profile", user.User)
-	admin.Post("/user/:id/lock", user.LockUser)
-	admin.Post("/user/:id/unlock", user.UnlockUser)
-
+func routeMiddlewareAfter(app *fiber.App) {
 	app.Use(filesystem.New(filesystem.Config{
-		Root: http.Dir("golang_api/storage"),
+		Root: http.Dir("api/storage"),
 	}))
 	app.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(404) // => 404 "Not Found"
 	})
+
 }
