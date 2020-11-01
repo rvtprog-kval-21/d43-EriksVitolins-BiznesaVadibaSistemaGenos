5c79ab7e7ba1c4a488f8c267ca8e017ccbb40862
Ēriks Vītoliņš <vitolinseriks@gmail.com>
2020-11-01 20:43:24 +0200

    added error page and lazy loading

---
+++ web/src/views/404/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-container>
+        <div class=" w-100 d-flex justify-content-center">
+            <img class="image d-flex justify-content-center" src="../../assets/Error.svg" alt="">
+        </div>
+        <div class="d-flex justify-content-center">
+            <h3>Page wasn't found</h3>
+        </div>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "notFound"
+    }
+</script>
+
+<style scoped>
+    .image{
+        width: 600px;
+        height: 400px;
+    }
+</style>
