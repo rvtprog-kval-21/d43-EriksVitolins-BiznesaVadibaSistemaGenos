959a472a4ed26d60f8d3212d667bd6e459041a61
Ēriks Vītoliņš <vitolinseriks@gmail.com>
2020-11-03 21:04:54 +0200

    finishing migratinos

--- api/model/tags/tags.go
+++ api/model/tags/tags.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Tags struct {
-	ID uint	`gorm:"primaryKey;not null" json:"id"`
-	CreatedAt time.Time `json:"created_at" json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" json:"updated_at"`
+	ID        uint           `gorm:"primaryKey;not null" json:"id"`
+	CreatedAt time.Time      `json:"created_at" json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
-	UserOwner   user.User  `gorm:"foreignKey:Owner"`
-	Owner int `json:"owner"`
-	Name string `gorm:"index;not null" json:"id"`
-	Avatar string `json:"avatar"`
-	About string `json:"about"`
-	Private bool `json:"private"`
-	IsPublic bool `json:"is_public"`
+	UserOwner user.User      `gorm:"foreignKey:Owner"`
+	Owner     int            `json:"owner"`
+	Name      string         `gorm:"index;not null" json:"id"`
+	Avatar    string         `json:"avatar"`
+	About     string         `json:"about"`
+	Private   bool           `json:"private"`
+	IsPublic  bool           `json:"is_public"`
 }
